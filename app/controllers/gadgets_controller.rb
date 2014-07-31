a6d8387fd40c27b65a50e5a4eefff098e6a2e28c
Josep <apuratepp@gmail.com>
2014-07-31 11:10:22 +0200

    gadgets basic CRUD

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -2,4 +2,42 @@ class GadgetsController < ApplicationController
 	def index
 		@gadgets = current_user.gadgets.includes(:pictures)
 	end
+	def new
+		@gadget = current_user.gadgets.new
+	end
+	def create
+		@gadget = current_user.gadgets.new(gadget_params)
+		if @gadget.save
+			redirect_to @gadget
+		else
+			render action: 'new'
+		end
+	end
+	def show
+		@gadget = current_user.gadgets.find(params[:id])
+	end
+	def edit
+		@gadget = current_user.gadgets.find(params[:id])
+	end
+	def update
+		@gadget = current_user.gadgets.find(params[:id])
+		if @gadget.update_attributes(gadget_params)
+			redirect_to @gadget
+		else
+			render action: 'edit'
+		end		
+	end
+	def destroy
+		@gadget = current_user.gadgets.find(params[:id])
+		if @gadget.destroy
+			redirect_to gadgets_path
+		else
+			redirect_to @gadget
+		end
+	end
+
+	private
+	def gadget_params
+		params.require(:gadget).permit(:name)
+	end
 end
--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -1,4 +1,6 @@
 class Gadget < ActiveRecord::Base
 	belongs_to :user
 	has_many   :pictures, :dependent => :delete_all
+
+	validates :name, presence: true
 end
