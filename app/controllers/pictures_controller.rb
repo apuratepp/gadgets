f008637891ce5f13b0bf277f1864d83f96418b86
Josep <apuratepp@gmail.com>
2014-07-31 11:48:49 +0200

    Basic Picture CRUD

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,29 @@
 class PicturesController < ApplicationController
+	def new
+		@picture = current_gadget.pictures.new
+	end
+	def create
+		@picture = current_gadget.pictures.new(picture_params)
+		if @picture.save
+			redirect_to current_gadget
+		else
+			render action: 'new'
+		end
+	end
+	def show
+		@picture = current_gadget.pictures.find(params[:id])
+	end
+	def destroy
+		@picture =  current_gadget.pictures.find(params[:id])
+		@picture.destroy
+		redirect_to current_gadget
+	end
+
+	private
+	def picture_params
+		params.require(:picture).permit(:image)
+	end
+	def current_gadget
+		current_user.gadgets.find(params[:gadget_id])
+	end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,7 @@
 class Picture < ActiveRecord::Base
 	belongs_to :gadget
+
+	# paperclip
+	has_attached_file :image, :default_url => "/images/missing.png"
+  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
